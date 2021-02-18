f87e0b0a63372589b67dce90396b16c2b0c258c3
veggieemmy <emmyangelamoss@gmail.com>
2021-02-18 11:10:15 -0600

    updated the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes =`0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response) {
 console.log(response.data);
 let temperatureElement = document.querySelector("#temperature")
@@ -10,6 +25,8 @@ let humidityElement = document.querySelector("#humidity")
 humidityElement.innerHTML = response.data.main.humidity;
 let windElement = document.querySelector("#wind")
 windElement.innerHTML = Math.round(response.data.wind.speed); 
+let dateElement = document.querySelector("#date")
+dateElement.innerHTML = formatDate(response.data.dt*1000);
 }
 
 let apiKey = "bd61a16d03e69c4265d6aac8396e35c8";
